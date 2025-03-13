9eb24bf501a71a5c265baaedc2d069f2bf18fd10
Bramasta <bramastasaka9@gmail.com>
2025-03-13 18:36:58 +0700

    add splash screen

---
+++ lib/onboarding/boardingone.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BoardingOne extends StatelessWidget {
+  const BoardingOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('sasa');
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'onboarding/boardingone.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AnimatedSplashScreenWidget extends StatelessWidget {
+  const AnimatedSplashScreenWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      duration: 4000,
+      splash: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Color(0xFFE997C6)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset(
+                'icon/splashanimation.json',
+                width: 300,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'FlottieCook',
+                style: GoogleFonts.agbalumo(fontSize: 40),
+              ),
+            ],
+          ),
+        ),
+      ),
+      nextScreen: const BoardingOne(),
+      splashTransition: SplashTransition.fadeTransition,
+      backgroundColor: Colors.transparent,
+      splashIconSize: double.infinity,
+    );
+  }
+}
